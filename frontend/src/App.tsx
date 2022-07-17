cdda735d2701c79b224a05b4563181570e24d14a
MarcosGOFavaretto <marcosfavaretto.dev@gmail.com>
2022-07-17 16:10:19 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,12 @@
+import NotificationButton from "./components/NotificationButton";
+
 function App() {
   return (
-    <h1>Olá mundo!</h1>
-  )
+    <> {/* Isso é um Fragment */}
+      <h1>Olá mundo!</h1>
+      <NotificationButton />
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/components/NotificationButton/index.tsx
@@ -0,0 +1,12 @@
+import icon from '../../assets/img/notification-icon.svg'
+import './styles.css'
+
+function NotificationButton() {
+  return (
+    <div className="dsmeta-red-btn"> {/* Deve-se sempre utilizar o "className" ao invés do "class" pois o último é uma palavra chave do javascript */}
+      <img src={icon} alt="Notificar" /> {/* Usa-se "{apelido}" para referenciar o import */}
+    </div>
+  );
+}
+
+export default NotificationButton;
