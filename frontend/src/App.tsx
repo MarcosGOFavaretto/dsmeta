863cd9a7e0ff16e407200276826da77c99d357fe
MarcosGOFavaretto <marcosfavaretto.dev@gmail.com>
2022-07-17 16:27:49 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,19 @@
-import NotificationButton from "./components/NotificationButton";
+import Header from "./components/Header";
+import SalesCard from "./components/SalesCard";
 
 function App() {
   return (
-    <> {/* Isso é um Fragment */}
-      <h1>Olá mundo!</h1>
-      <NotificationButton />
+    <>
+      {" "}
+      {/* Isso é um Fragment e serve para utilizar mais de um componente seguido do outro. */}
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container"></div>
+
+          <SalesCard />
+        </section>
+      </main>
     </>
   );
 }
