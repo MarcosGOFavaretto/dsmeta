0e73c6f2e314f0d00eba4d2ac41d2509727e93e8
MarcosGOFavaretto <marcosfavaretto.dev@gmail.com>
2022-07-17 21:55:01 -0300

    Send notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,10 +1,25 @@
-import icon from '../../assets/img/notification-icon.svg';
-import './styles.css';
+import axios from "axios";
+import icon from "../../assets/img/notification-icon.svg";
+import { BASE_URL } from "../../utils/request";
+import "./styles.css";
 
-function NotificationButton() {
+type Props = {
+  saleId: number;
+};
+
+function handleClick(id: number) {
+  axios(`${BASE_URL}/sales/${id}/notification`).then((response) => {
+    console.log("SUCESSO");
+  });
+}
+
+function NotificationButton({ saleId }: Props) {
   return (
-    <div className="dsmeta-red-btn"> {/* Deve-se sempre utilizar o "className" ao invés do "class" pois o último é uma palavra chave do javascript */}
-      <img src={icon} alt="Notificar" /> {/* Usa-se "{apelido}" para referenciar o import */}
+    <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
+      {" "}
+      {/* Deve-se sempre utilizar o "className" ao invés do "class" pois o último é uma palavra chave do javascript */}
+      <img src={icon} alt="Notificar" />{" "}
+      {/* Usa-se "{apelido}" para referenciar o import */}
     </div>
   );
 }
